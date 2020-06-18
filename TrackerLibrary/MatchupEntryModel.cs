15d47ca17d519fc0edcbb0f5e5c037b7941f730f
CDinuwan <chanukadinuwan35@gmail.com>
2020-06-18 21:11:22 +0530

    Add tracker library today

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        public TeamModel TeamCompeting { get; set; }
+        public double Score { get; set; }
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
